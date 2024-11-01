91a7eadb04f4918c31c45c6df410aa00c221486a
kenji-yt <kenjiyiptong@gmail.com>
2024-11-01 20:27:55 +0100

    some progress

--- R/plot_utils.R
+++ R/plot_utils.R
@@ -6,7 +6,7 @@
 #' @param n_cores Number of cores to use ('1' by default).
 #' @param prog_ploidy Ploidy of the progenitors (Assumed to be equal. '2' by default)
 #' @param plot_cn Logical: plot a line indicating infered copy number ('TRUE' by default in CN has been estimated).
-#' @param add_bins Logical: plot counts for each bin (normalized in plot_cn=TRUE).
+#' @param add_bins Logical: plot counts for each bin ('TRUE' by default; normalized in plot_cn=TRUE).
 #' @param colour_map A vector of colours for each progenitor ('c(purple,orange)' by default).
 #' @param specific_chr A vector of characters indicating which chromosomes to plot (plots all by default).
 #' @param return_list Logical: return a list of plots if quick_view_sample.
@@ -146,17 +146,16 @@ plot_bins <- function(heal_list, quick_view_sample=FALSE, output_dir=FALSE, n_co
 
 
 
-#' Title
+#' Plotting function for heal alignment
 #'
 #' @param heal_list
-#' @param aln_map
-#' @param show_dtw_aligned
+#' @param genespace_dir
 #' @param quick_view_sample
 #' @param output_dir
 #' @param n_cores
-#' @param prog_ploidy
-#' @param plot_cn
-#' @param add_bins
+#' @param only_anchors
+#' @param add_bins Logical: plot counts for each bin ('TRUE' by default).
+#' @param prog_ploidy Ploidy of the progenitors (Assumed to be equal. '2' by default)
 #' @param colour_map
 #' @param specific_chr
 #' @param return_list
@@ -165,22 +164,16 @@ plot_bins <- function(heal_list, quick_view_sample=FALSE, output_dir=FALSE, n_co
 #' @export
 #'
 #' @examples
-plot_alignment <- function(heal_list, aln_map, show_dtw_aligned=TRUE, quick_view_sample=FALSE, output_dir=FALSE, n_cores=1, prog_ploidy=2, plot_cn=TRUE, add_bins=TRUE, colour_map=c("purple","orange"), specific_chr=FALSE, return_list=FALSE){
+plot_alignment <- function(heal_list, genespace_dir, quick_view_sample=FALSE, output_dir=FALSE, n_cores=1, add_bins=TRUE, prog_ploidy=2, colour_map=c("purple","orange"), specific_chr=FALSE, return_list=FALSE){
 
-  cn_exist <- sum(names(heal_list[[1]])=="CN")!=0
-  if(cn_exist!=TRUE){
-    cat("ERROR: no CN data. Exiting...")
-    return()
-  }
+  alignment_cn <- get_cn_alignment_by_anchors(heal_list, genespace_dir, n_cores)
+  polyploid_samples <- names(alignment_cn)
 
   sample_averages <- get_sample_stats(heal_list)
+
   progenitors <- names(heal_list)
 
   names(colour_map) <- progenitors
-
-  smp_type_map <- get_sample_stats(heal_list,sample_type = TRUE)
-
-  polyploid_samples <- smp_type_map$sample[smp_type_map$type=="polyploid"]
 
   if(quick_view_sample!=FALSE){
 
@@ -201,7 +194,7 @@ plot_alignment <- function(heal_list, aln_map, show_dtw_aligned=TRUE, quick_view
     cat(paste0("Plotting all samples and chromosomes to ",output_dir,"."))
   }
 
-  for(smp in polyploid_samples){
+  foreach::foreach(smp=polyploid_samples)%do%{
 
     if(output_dir!=FALSE){
       smp_dir <- paste0(output_dir,"/",smp,"/")
@@ -231,83 +224,50 @@ plot_alignment <- function(heal_list, aln_map, show_dtw_aligned=TRUE, quick_view
           out_file <- paste0(ref_dir,"/",chr,".png")
         }
 
-        which_rows <- heal_list[[ref]]$bins$chr==chr
+        ref_chr_col_name <- paste0("chr_", ref)
+        ref_start_col_name <- paste0("start_", ref)
+        ref_end_col_name <- paste0("end_", ref)
+        ref_cn_col_name <- paste0("cn_", ref)
 
-        x <- heal_list[[ref]]$bins$start[which_rows]
-        y_pts <- heal_list[[ref]]$bins[[smp]][which_rows]
+        which_rows_aln_dt <- alignment_cn[[smp]][[ref_chr_col_name]]==chr
+
+        x <- (alignment_cn[[smp]][[ref_start_col_name]][which_rows_aln_dt] + alignment_cn[[smp]][[alt_end_col_name]][which_rows_aln_dt]) / 2
+        y_line <- alignment_cn[[smp]][[ref_cn_col_name]][which_rows_aln_dt]
         subgnm_group <- rep(ref, length(x))
 
-        if(plot_cn==TRUE){
+        for(alt in alt_gnms){
 
-          y_line <- heal_list[[ref]]$CN[[smp]][which_rows]
+          alt_start_col_name <- paste0("start_", alt)
+          alt_end_col_name <- paste0("end_", alt)
+          alt_cn_col_name <- paste0("cn_", alt)
 
-          for(alt in alt_gnms){
+          x_alt <- (alignment_cn[[smp]][[alt_start_col_name]][which_rows_aln_dt] + alignment_cn[[smp]][[alt_end_col_name]][which_rows_aln_dt]) / 2
+          y_alt <- alignment_cn[[smp]][[alt_cn_col_name]][which_rows_aln_dt]
 
-            which_alt_row <- aln_map[[ref]][[alt]]$ref_chr==chr
+          subgnm_group <- c(subgnm_group, rep(alt, length(y_alt)))
+          x <- c(x, x_alt) # same coordinates
+          y_line <- c(y_line, y_alt)
+        }
 
-            subgnm_group <- c(subgnm_group, rep(alt, sum(which_alt_row)))
+        lines_df <- data.frame(start=x, copy=y_line, subgenome=subgnm_group)
 
-            sub_map <- aln_map[[ref]][[alt]][which_alt_row,]
+        if(add_bins==TRUE){
 
-            x <- c(x, sub_map$ref_bin)
+          which_rows_bins_dt <- heal_list[[ref]]$bins$chr==chr
 
-            for(i in 1:nrow(sub_map)){
-              chr_alt <- sub_map$alt_chr[i]
-              pos_alt <- sub_map[[smp]][i]
-              which_row <- heal_list[[alt]]$CN$chr==chr_alt & heal_list[[alt]]$CN$start==pos_alt
-              y_line <- c(y_line,  heal_list[[alt]]$CN[[smp]][which_row])
+          x_pts <- heal_list[[ref]]$bins$start[which_rows_bins_dt]
+          y_pts <- heal_list[[ref]]$bins[[smp]][which_rows_bins_dt]
+          y_pts <- (y_pts/sample_averages[[smp]])*prog_ploidy
 
-              if(add_bins==TRUE){
-                which_row <- heal_list[[alt]]$bins$chr==chr_alt & heal_list[[alt]]$bins$start==pos_alt
-                y_pts <- c(y_pts,  heal_list[[alt]]$bins[[smp]][which_row])
-              }
-            }
-          }
-
-
-          if(add_bins==TRUE){
-
-            y_pts <- (y_pts/sample_averages[[smp]])*prog_ploidy
-
-            plot_df <- data.frame(start=x,counts=y_pts,copy=y_line,subgenome=subgnm_group)
-
-            bin_plot <- ggplot2::ggplot() +
-              ggplot2::geom_line(data = plot_df, ggplot2::aes(x = x, y = copy, colour=subgenome), linewidth = 2) +
-              ggplot2::geom_point(data = plot_df, ggplot2::aes(x = x, y = counts, colour=subgenome), size = 1, alpha = 0.1) +
-              ggplot2::theme_minimal() +
-              ggplot2::scale_color_manual(values = colour_map) +
-              ggplot2::ylim(0, 8)+
-              ggplot2::labs(title = paste(chr, ref, smp), x = "Position", y = "Copy Number") +
-              ggplot2::theme_bw()
-
-            if (show_dtw_aligned==TRUE) {
-              smp_col_name <- paste0("aligned_",smp)
-              which_row <- aln_map[[ref]][[alt]]$ref_chr==chr
-              which_dtw_count <- aln_map[[ref]][[alt]][[smp_col_name]][which_row]=="dtw_counts"
-              dtw_count_zones <- aln_map[[ref]][[alt]]$ref_bin[which_row][which_dtw_count]
-              bin_plot <- bin_plot + ggplot2::geom_vline(xintercept = dtw_count_zones, color = "red", alpha=0.05)
-
-              which_dtw_cn <- aln_map[[ref]][[alt]][[smp_col_name]][which_row]=="dtw_cn"
-              dtw_cn_zones <- aln_map[[ref]][[alt]]$ref_bin[which_row][which_dtw_cn]
-              bin_plot <- bin_plot + ggplot2::geom_vline(xintercept = dtw_cn_zones, color = "purple", alpha=0.05)
-            }
-
-            if(output_dir!=FALSE){
-              ggplot2::ggsave(filename=out_file,bin_plot,device="png", width = 6, height = 4, units = "in")
-            }else{
-              return(bin_plot)
-            }
-
-        }else{
-
-          plot_df <- data.frame(start=x,copy=y_line,subgenome=subgnm_group)
+          pts_df <- data.frame(start=x_pts, points=y_pts)
 
           bin_plot <- ggplot2::ggplot() +
-            ggplot2::geom_line(data = plot_df, ggplot2::aes(x = x, y = copy, colour=subgenome), linewidth = 2) +
+            ggplot2::geom_line(data = lines_df, ggplot2::aes(x = start, y = copy, colour=subgenome), linewidth = 2) +
+            #ggplot2::geom_point(data = pts_df, ggplot2::aes(x = start, y = points, colour=colour_map[[ref]]), size = 1, alpha = 0.1) +
             ggplot2::theme_minimal() +
             ggplot2::scale_color_manual(values = colour_map) +
             ggplot2::ylim(0, 8)+
-            ggplot2::labs(title = paste(chr,smp), x = "Position", y = "Copy Number") +
+            ggplot2::labs(title = paste(chr, ref, smp), x = "Position", y = "Copy Number") +
             ggplot2::theme_bw()
 
           if(output_dir!=FALSE){
@@ -315,16 +275,15 @@ plot_alignment <- function(heal_list, aln_map, show_dtw_aligned=TRUE, quick_view
           }else{
             return(bin_plot)
           }
-        }
-      }else{
 
-          plot_df <- data.frame(start=x,counts=y_pts, progenitor=rep(prog,length(y_pts)))
+        }else{
 
           bin_plot <- ggplot2::ggplot() +
-            ggplot2::geom_point(data = plot_df, ggplot2::aes(x = x, y = counts, colour=progenitor), size = 1, alpha = 1) +
+            ggplot2::geom_line(data = lines_df, ggplot2::aes(x = start, y = copy, colour=subgenome), linewidth = 2) +
             ggplot2::theme_minimal() +
             ggplot2::scale_color_manual(values = colour_map) +
-            ggplot2::labs(title = paste(c,ref), x = "Position", y = "Counts") +
+            ggplot2::ylim(0, 8)+
+            ggplot2::labs(title = paste(chr, smp, ref), x = "Position", y = "Copy Number") +
             ggplot2::theme_bw()
 
           if(output_dir!=FALSE){
@@ -335,6 +294,7 @@ plot_alignment <- function(heal_list, aln_map, show_dtw_aligned=TRUE, quick_view
         }
       }
       doParallel::stopImplicitCluster()
+
       if(quick_view_sample!=FALSE){
         if(return_list==TRUE){
           names(plot_list) <- chromo
